c9ed3efc6a1088f643a3ed6fb8b01eb523645fe8
vetatlas <abdullahkarabiber31@gmail.com>
2026-04-27 00:01:17 +0300

    Delete arena.js

--- arena.js
+++
@@ -1,8 +0,0 @@
-function renderArena() {
-    document.getElementById('view-arena').innerHTML = `
-        <div class="premium-card">
-            <h2><i class="fas fa-brain"></i> Sınav Arena</h2>
-            <p>Premium test modülü yakında...</p>
-        </div>
-    `;
-}
